8ab525c118dd9e7f6d6903426145a31684ca4f31
ASUS <ASUS@DESKTOP-M4843TO>
2018-06-06 16:30:53 +0700

    update check 30xe/ngay

--- QLGaraGUI/TiepNhanXeSuaGUI.vb
+++ QLGaraGUI/TiepNhanXeSuaGUI.vb
@@ -2,35 +2,67 @@
 Imports QLGaraDTO
 Imports Utility
 Imports QLGaraDAL
+Imports System.Data.SqlClient
+
 Public Class TiepNhanXeSuaGUI
     Private xeBus As TiepNhanXeSuaBUS
     Private lhxBus As LoaiHieuXeBus
 
     Private Sub tiepnhan_Click(sender As Object, e As EventArgs) Handles btTiepNhanXe.Click
+
         Dim XE As TiepNhanXeSuaDTO
         XE = New TiepNhanXeSuaDTO()
+        'check so xe trong ngay
+        Dim xemax As Integer
+        xemax = 30
+        Dim xecount As Integer
+        Dim query As String = String.Empty
+        query &= "select COUNT([NGAYTIEPNHAN])"
+        query &= "from [TIEPNHAN]"
+        query &= "where cast([NGAYTIEPNHAN] as Date) = cast(getdate() as Date)"
 
-        XE.BienSo1 = tbBienSo.Text
-        XE.DiaChi1 = tbDiachi.Text
-        XE.DienThoai1 = tbDienThoai.Text
-        XE.HieuXe1 = cbHieuXe.Text
-        XE.NgayTiepNhan1 = dtpNgayTiepNhan.Value
-        XE.TenChuXe1 = tbTenChuXe.Text
-
-
-        '2. Business .....
-        xeBus = New TiepNhanXeSuaBUS()
-
-
-        '3. Insert to DB
-        Dim result As Result
-        result = xeBus.insert(XE)
-        If (result.FlagResult = True) Then
-            MessageBox.Show("Thêm xe thành công.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
-
+        Using conn As New SqlConnection("Data Source=DESKTOP-M4843TO\SQLEXPRESS;Initial Catalog=VBPROJECT;Integrated Security=True")
+            Using comm As New SqlCommand()
+                With comm
+                    .Connection = conn
+                    .CommandType = CommandType.Text
+                    .CommandText = query
+                End With
+                Try
+                    conn.Open()
+                    xecount = Convert.ToInt32(comm.ExecuteScalar())
+                Catch ex As Exception
+                    conn.Close()
+                    System.Console.WriteLine(ex.StackTrace)
+                End Try
+            End Using
+        End Using
+        If xecount > xemax Then
+            MessageBox.Show("Đã tiếp nhận quá 30 xe trong ngày")
         Else
-            MessageBox.Show("Thêm xe không thành công.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            System.Console.WriteLine(result.SystemMessage)
+            XE.BienSo1 = tbBienSo.Text
+            XE.DiaChi1 = tbDiachi.Text
+            XE.DienThoai1 = tbDienThoai.Text
+            XE.HieuXe1 = cbHieuXe.Text
+            XE.NgayTiepNhan1 = dtpNgayTiepNhan.Value
+            XE.TenChuXe1 = tbTenChuXe.Text
+
+
+            '2. Business .....
+            xeBus = New TiepNhanXeSuaBUS()
+
+
+            '3. Insert to DB
+            Dim result As Result
+            result = xeBus.insert(XE)
+            If (result.FlagResult = True) Then
+                MessageBox.Show("Thêm xe thành công.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            Else
+                MessageBox.Show("Thêm xe không thành công.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                System.Console.WriteLine(result.SystemMessage)
+            End If
+
         End If
     End Sub
 
@@ -51,11 +83,6 @@ Public Class TiepNhanXeSuaGUI
         cbHieuXe.DataSource = New BindingSource(listLoaiHieuXe, String.Empty)
         cbHieuXe.DisplayMember = "strTenLoaiXe1"
         cbHieuXe.ValueMember = "strTenLoaiXe1"
-
-
-        'check so xe trong ngay
-        Dim xemax As Integer
-        xemax = 1
 
 
 
